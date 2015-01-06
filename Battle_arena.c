19ff99348e2ca3f3f3019c949753eafedc14d69e
Kaelios <rrooxxaass@hotmail.fr>
2015-01-06 18:27:10 +0100

    compte rendu

--- Battle_arena.c
+++ Battle_arena.c
@@ -4,14 +4,14 @@
 *	\file Battle_arena.c	\brief Main du Battle Arena avec quelque fonctions
 */
 #include "fonctions.h"
-#include "fonctionsgrph.h"
+#include "fonctionsgrph.c"
+#include "SDL/SDL.h"
 t_perso arena[N][N];
 int tour;
 
 //----------------------------------------------------------------------------------------------LE-MAIN---------------------------------------------------------------
 int main(){
 	int charger=0;
-	int winner=0;
 
 	printf("Choix du mode d'affichage: \n 1 = mode graphique\n 2 = mode terminal\n\nVotre choix?\n");
 	while((charger<1)&&(charger>2))
@@ -34,5 +34,5 @@ int main(){
 	
 		gest_pers();
 	}
-	return EXIT_SUCCESS;
+	return 0;
 }
